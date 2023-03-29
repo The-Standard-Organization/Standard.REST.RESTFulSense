9e2d69a295877fb503b6dd4627f84ac4a0d16b5f
Christo du Toit <christo@dutoit.co.uk>
2023-03-30 00:06:45 +0100

    INFRA: Initialize Build Project

---
+++ Standard.API.RESTFulSense.Infrastructure.Build/Program.cs
@@ -0,0 +1,13 @@
+﻿using Standard.API.RESTFulSense.Infrastructure.Build.Services;
+
+namespace Standard.API.RESTFulSense.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
---
+++ Standard.API.RESTFulSense.Infrastructure.Build/Services/ScriptGenerationService.cs
@@ -0,0 +1,92 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace Standard.API.RESTFulSense.Infrastructure.Build.Services
+{
+    internal class ScriptGenerationService
+    {
+        private readonly ADotNetClient adotNetClient;
+
+        public ScriptGenerationService() =>
+            this.adotNetClient = new ADotNetClient();
+
+        public void GenerateBuildScript()
+        {
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Standard.API.RESTFulSense Build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Pulling Code"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Installing .NET",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.201",
+                                    IncludePrerelease = true
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring Packages"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building Solution"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Running Tests"
+                            }
+                        }
+                    }
+                }
+            };
+
+            string yamlRelativeFilePath = "../../../../.github/workflows/build.yml";
+            string yamlFullPath = Path.GetFullPath(yamlRelativeFilePath);
+            FileInfo yamlDefinition = new FileInfo(yamlFullPath);
+
+            if (!yamlDefinition.Directory.Exists)
+            {
+                yamlDefinition.Directory.Create();
+            }
+
+            adotNetClient.SerializeAndWriteToFile(
+                adoPipeline: githubPipeline,
+                path: yamlRelativeFilePath);
+        }
+    }
+}
+
