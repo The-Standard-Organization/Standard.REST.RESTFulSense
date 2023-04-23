b7d2eb8820a86c97503b7ec16aa4c6482bf91b73
Christo du Toit <christo@dutoit.co.uk>
2023-04-23 02:11:31 +0100

    DATA: Status Codes

---
+++ Standard.REST.RESTFulSense/Models/HttpStatusCodes/StatusCode.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) 2023 - The Standard Community - All rights reserved.
+// ---------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Standard.REST.RESTFulSense.Models.HttpStatusCodes
+{
+    internal class StatusCode
+    {
+        [JsonProperty("statusCode")]
+        public int Code { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
