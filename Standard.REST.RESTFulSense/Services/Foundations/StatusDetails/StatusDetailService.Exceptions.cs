71d1c03d15cac68fa6a2b7b8c835bd579c38f148
Christo du Toit <christo@dutoit.co.uk>
2023-04-26 00:52:56 +0100

    ShouldThrowDependencyExceptionOnRetrieveStatusDetailByCodeIfErrorOccurs -> PASS

--- Standard.REST.RESTFulSense/Services/Foundations/StatusDetails/StatusDetailService.Exceptions.cs
+++ Standard.REST.RESTFulSense/Services/Foundations/StatusDetails/StatusDetailService.Exceptions.cs
@@ -119,6 +119,83 @@ namespace Standard.REST.RESTFulSense.Services.Foundations.StatusDetails
             {
                 throw CreateAndLogValidationException(notFoundStatusDetailException);
             }
+            catch (JsonReaderException jsonReaderException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(jsonReaderException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (JsonSerializationException jsonSerializationException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(jsonSerializationException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (JsonException jsonException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(jsonException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (ArgumentNullException argumentNullException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(argumentNullException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (ArgumentException argumentException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(argumentException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (PathTooLongException pathTooLongException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(pathTooLongException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (DirectoryNotFoundException directoryNotFoundException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(directoryNotFoundException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (FileNotFoundException fileNotFoundException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(fileNotFoundException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (UnauthorizedAccessException unauthorizedAccessException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(unauthorizedAccessException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (NotSupportedException notSupportedException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(notSupportedException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
+            catch (IOException iOException)
+            {
+                var failedStatusDetailStorageException =
+                    new FailedStatusDetailStorageException(iOException);
+
+                throw CreateAndLogDependencyException(failedStatusDetailStorageException);
+            }
         }
 
         private StatusDetailDependencyException CreateAndLogDependencyException(Xeption exception)
