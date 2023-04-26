2b1e689d2b678670d9f98e1bd1d2d59df7d08b11
Christo du Toit <christo@dutoit.co.uk>
2023-04-26 17:59:28 +0100

    BROKERS: Serialization Broker - Serialize

---
+++ Standard.REST.RESTFulSense/Brokers/Serializations/ISerializationBroker.cs
@@ -0,0 +1,13 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) - The Standard Community - All rights reserved.
+// -------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Standard.REST.RESTFulSense.Brokers.Serializations
+{
+    internal interface ISerializationBroker
+    {
+        string Serialize<T>(T obj, JsonSerializerSettings settings = null);
+    }
+}
