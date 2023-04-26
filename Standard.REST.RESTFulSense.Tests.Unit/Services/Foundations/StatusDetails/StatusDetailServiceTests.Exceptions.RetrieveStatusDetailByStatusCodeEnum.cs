af5e9041626806271a1fe878ed4b086720969ded
Christo du Toit <christo@dutoit.co.uk>
2023-04-26 01:17:52 +0100

    ShouldThrowDependencyExceptionOnRetrieveStatusDetailByCodeEnumIfErrorOccurs -> PASS

---
+++ Standard.REST.RESTFulSense.Tests.Unit/Services/Foundations/StatusDetails/StatusDetailServiceTests.Exceptions.RetrieveStatusDetailByStatusCodeEnum.cs
@@ -0,0 +1,54 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) - The Standard Community - All rights reserved.
+// -------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Moq;
+using Standard.REST.RESTFulSense.Models.Foundations.StatusDetails;
+using Standard.REST.RESTFulSense.Models.Foundations.StatusDetails.Exceptions;
+using Xunit;
+
+namespace Standard.REST.RESTFulSense.Tests.Unit.Services.Foundations.StatusDetails
+{
+    public partial class StatusDetailServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public void ShouldThrowDependencyExceptionOnRetrieveStatusDetailByCodeEnumIfErrorOccurs(
+            Exception dependancyException)
+        {
+            // given
+            int randomNumber = GetRandomNumber();
+            int someStatusCode = 200 + randomNumber;
+            HttpStatusCode someHttpStatusCode = (HttpStatusCode)(someStatusCode);
+
+            var failedStorageException =
+                new FailedStatusDetailStorageException(dependancyException);
+
+            var expectedStatusDetailDependencyException =
+                new StatusDetailDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStatusDetails())
+                    .Throws(dependancyException);
+
+            // when
+            Action retrieveStatusDetailByCodeAction = () =>
+                this.statusDetailService.RetrieveStatusDetailByCode(someHttpStatusCode);
+
+            StatusDetailDependencyException actualStatusDetailDependencyException =
+                Assert.Throws<StatusDetailDependencyException>(retrieveStatusDetailByCodeAction);
+
+            // then
+            actualStatusDetailDependencyException.Should()
+                .BeEquivalentTo(expectedStatusDetailDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStatusDetails(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
